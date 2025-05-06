0e8ba668d6b6313d1d6dd8b496e9d13503e25aa5
Amani66j2123 <a7960317@gmail.com>
2025-05-07 00:16:40 +0400

    location update

--- client/src/Components/Posts.js
+++ client/src/Components/Posts.js
@@ -1,15 +1,14 @@
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { getPosts } from "../Features/PostSlice";
+import { getPosts, likePost } from "../Features/PostSlice"; // Adjust path as needed
 import { Table } from "reactstrap";
 import moment from "moment";
-
-import { likePost } from "../Features/PostSlice";
 import { FaThumbsUp } from "react-icons/fa6";
 
 const Posts = () => {
   const posts = useSelector((state) => state.posts.posts);
+  const loading = useSelector((state) => state.posts.loading);
   const email = useSelector((state) => state.users.user.email);
   const userId = useSelector((state) => state.users.user._id);
 
@@ -18,29 +17,33 @@ const Posts = () => {
 
   useEffect(() => {
     dispatch(getPosts());
-  }, []);
+  }, [dispatch]);
 
   const handleLikePost = (postId) => {
-    const postData = {
-      postId: postId,
-      userId: userId,
-    };
+    const postData = { postId, userId };
     dispatch(likePost(postData));
     navigate("/");
   };
 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="postsContainer">
       <Table className="table table-striped">
-        <thead></thead>
+        <thead>
+          <tr>
+            <th>Email</th>
+            <th>Post</th>
+          </tr>
+        </thead>
         <tbody>
-          {posts.map((post) => (
+          {posts?.map((post) => (
             <tr key={post._id}>
-              {/* Ensure to add a unique key for each row */}
               <td>{post.email}</td>
               <td>
-                {/*<p>{post.createdAt}</p>*/}
-                <p> {moment(post.createdAt).fromNow()}</p>
+                <p>{moment(post.createdAt).fromNow()}</p>
                 {post.postMsg}
                 <p className="likes">
                   <a href="#" onClick={() => handleLikePost(post._id)}>
@@ -53,7 +56,7 @@ const Posts = () => {
           ))}
         </tbody>
       </Table>
-    </div> /* End of posts */
+    </div>
   );
 };
 
